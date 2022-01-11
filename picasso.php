49a999a309c14713d1b096d24213a58065a7923b
WataruNishimura <wataru.nishimura@nnkk.work>
2022-01-11 14:36:44 +0000

    first commit

---
+++ picasso.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Picasso
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     picasso
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Picasso
+ */
+
+// Your code starts here.
