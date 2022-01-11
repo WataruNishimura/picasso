fbffbf833cc8acb3d1798af4ab0172ba9d8df431
WataruNishimura <wataru.nishimura@nnkk.work>
2022-01-11 14:40:47 +0000

    add php info

--- picasso.php
+++ picasso.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     Picasso
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://github.com/crafts-digitally/picasso
+ * Description:     Generate post thumbnail based on specific templates.
+ * Author:          WATARU NISHIMURA
+ * Author URI:      https://twitter.com/wataru_nishi731
  * Text Domain:     picasso
  * Domain Path:     /languages
  * Version:         0.1.0
