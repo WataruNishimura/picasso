f36447c40d8e901cc226b55953736935d463cd51
WataruNishimura <wataru.nishimura@kraftsman.jp>
2022-01-21 09:23:06 +0000

    Generate image by specified text and load font data

--- images.php
+++ images.php
@@ -1,8 +1,28 @@
 <?php
+
+require("vendor/autoload.php");
+
+$text = $_GET["text"];
+
+if($text == "") {
+  $text = "This image is generate by picasso.";
+}
+
+$fontfile = "./assets/fonts/Roboto/Roboto-Black.ttf";
+
+$font =  FontLib\Font::load($fontfile);
+$font->parse();
+
+$bbox = imagettfbbox(80, 0, $fontfile, $text . "\n" . $font->getFontName());
+$bbox_width = $bbox[4] - $bbox[6];
+$bbox_height = $bbox[5] - $bbox[7];
+
 header("Content-Type: image/jpeg");
-$im     = imagecreatetruecolor(1920, 1080);
-$white = imagecolorallocate($im, 220, 200, 100);
+$im  = imagecreatetruecolor(1920, 1080);
+$white = imagecolorallocate($im, 255, 255, 255);
+$black = imagecolorallocate($im, 0, 0, 0);
 imagefill($im, 0, 0, $white);
+imagettftext($im, 80, 0, (1920 - $bbox_width) / 2 , (1080 - $bbox_height) / 2, $black, $fontfile, $text . "\n" . $font->getFontName());
 imagejpeg($im);
 imagedestroy($im);
 
--- lib/generator.php
+++ lib/generator.php
@@ -1,21 +1,12 @@
 <?php 
 
-function generate_post_thumbnail(string $text, GdImage $background, int $text_color, $font = null) : GdImage {
-  
-  $thumbnail_width = 1920;
-  $thumbnail_height = 1080;
-
-  $thumbnail_base = imagecreatetruecolor(
-    1920, 1080
-  );
+function generate_post_thumbnail(string $text, GdImage $background, int $text_color, $font = null) {
 
   if($font == null) {
     $font = 1;
   }
 
-  imagestring($thumbnail_base, $font, $thumbnail_width / 2, $thumbnail_height / 2, $text, $text_color);
-
-  return $thumbnail_base;
+  imagestring($background, $font, 1920 / 2, 1080 / 2, $text, $text_color);
 }
 
 ?>
